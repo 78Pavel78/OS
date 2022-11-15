9da422a16a7500f8e6d8c88f71fac230d01cc39c
78Pavel78 <112005061+78Pavel78@users.noreply.github.com>
2022-11-16 01:04:11 +0300

    Add files via upload

---
+++ OS/os.js
@@ -0,0 +1,11 @@
+import { homedir } from 'os';
+console.log(homedir());
+
+import { hostname } from "os";
+console.log(hostname());
+
+import { type } from "os";
+console.log(type());
+
+import { version } from "os";
+console.log(version());
